2474a1e06fc6220cd2be8670191964084d259f34
John Peter <jpinaus@gmail.com>
2022-01-06 23:05:24 +1000

    Adding web server declaration

--- main.tf
+++ main.tf
@@ -18,3 +18,82 @@ resource "aws_dynamodb_table" "tfc_example_table" {
     type = "S"
   }
 }
+
+resource "aws_security_group" "web_server"{
+    name = "Allow inbound HTTP/HTTPS/SSH and any outbound"
+    
+    ingress = [{
+        description = "Allow HTTP from anywhere"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+        self = false
+        prefix_list_ids = []
+        security_groups = []
+    },
+    {
+        description = "Allow HTTPS from anywhere"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+        self = false
+        prefix_list_ids = []
+        security_groups = []
+    },
+    {
+        description = "Allow SSH from anywhere"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+        self = false
+        prefix_list_ids = []
+        security_groups = []
+    }]
+
+    egress {
+        description = "Allow all outbound traffic"
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+// Declare the EIP for wordpress site
+data "aws_eip" "wordpress_eip" {
+    id = "eipalloc-0c4c68616cd40a8de"
+}
+
+// Create an instance profile related to the role
+resource "aws_iam_instance_profile" "access_s3_profile" {
+    name  = "access_s3_profile"
+//    role = "${aws_iam_role.ec2_wordpress_role.name}"
+    role = "ec2_s3_access_wordpress"
+}
+
+// Create an EC2 with a AWS Linux 2 AMI
+resource "aws_instance" "EC2" {
+    ami = "ami-0ed9277fb7eb570c9"
+    instance_type = "t2.micro"
+    security_groups = [aws_security_group.web_server.name]
+
+    ebs_optimized = false
+    root_block_device {
+        volume_type = "gp2"
+        volume_size = 30
+    }
+    iam_instance_profile = "${aws_iam_instance_profile.access_s3_profile.name}"
+//    user_data = "${file("init.sh")}"
+}
+
+resource "aws_eip_association" "eip_association" {
+  instance_id   = aws_instance.EC2.id
+  allocation_id = data.aws_eip.wordpress_eip.id
+}
+
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
+output "EIP_public_IP_address" {
+    value = data.aws_eip.wordpress_eip.public_ip
+}
+
+output "EC2_private_IP_address" {
+    value = aws_instance.EC2.private_ip
+}
+
 output "tfc_example_table_arn" {
   value = aws_dynamodb_table.tfc_example_table.arn
 }
